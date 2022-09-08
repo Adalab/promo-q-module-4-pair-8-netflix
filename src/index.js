e399501c4f014d367ba3b96bc8e4544d0ed7a6a8
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-09-08 11:13:08 +0200

    add database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const listMovies = require("./data/movies.json");
+const Database = require('better-sqlite3');
 
 // create and config server
 const server = express();
@@ -13,22 +13,39 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
+const db = new Database('./src/db/database.db', { verbose: console.log});
+
 server.get("/movies", (req, resp) => {
+  const query = db.prepare(
+    `SELECT * 
+    FROM movies` 
+  )
+  const allMovies = query.all();
+  console.log(allMovies);
   resp.json({
-    success: true,
-    movies: listMovies
-  })
+    movies: allMovies,
+    success: true
+  });
 })
 
 server.set('view engine', 'ejs');
 
+
+
 server.get('/movie/:movieId', (req, res) => {
   console.log(req.params.movieId);
-  const foundMovie = listMovies.find((oneMovie) => parseInt(oneMovie.id) === parseInt(req.params.movieId));
+  const query = db.prepare(
+    `SELECT * 
+    FROM movies` 
+  )
+  const allMovies = query.all();
+  const foundMovie = allMovies.find((oneMovie) => parseInt(oneMovie.id) === parseInt(req.params.movieId));
   console.log(foundMovie);
 
   res.render('movie', foundMovie);
 })
+
+
 
 const staticServer = ('./src/public-react');
 server.use(express.static(staticServer));
